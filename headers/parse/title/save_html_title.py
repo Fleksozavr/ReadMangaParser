b9cf3a588107ec3e56179d9fcadf39c653876273
14Fleksss88 <131782632+Fleksozavr@users.noreply.github.com>
2024-05-18 23:03:29 +0500

    Добавил переменные окружения
    интегрировал переменные окружения из .env

--- headers/parse/title/save_html_title.py
+++ headers/parse/title/save_html_title.py
@@ -6,6 +6,11 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 class Title:
     def __init__(self):
@@ -15,9 +20,9 @@ class Title:
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)")
         options.add_argument('--headless=new')
-
-        self._browser = uc.Chrome(driver_executable_path='C:/Users/user/Desktop/manga/chromedriver/chromedriver.exe',
-                                  browser_executable_path='C:/Program Files/Google/Chrome/Application/chrome.exe',
+        
+        self._browser = uc.Chrome(driver_executable_path=os.getenv('DRIVER_PATH'),
+                                  browser_executable_path=os.getenv('CHROME_PATH'),
                                   disable_detection=True,
                                   options=options
                                   )
